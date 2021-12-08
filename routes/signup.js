53ee04ab656740e2c62649c5e033ef3b1f7f691c
jacksonkasi <jacksonkasi@outlook.com>
2021-12-08 20:14:02 +0530

    update

--- routes/signup.js
+++ routes/signup.js
@@ -26,16 +26,47 @@ router.post("/signup", async (req, res) => {
           from: process.env.MAIL,
           to: req.body.email,
           subject: "Verify your email - Meta",
+          attachments: [
+            {
+              filename: "meta.png",
+              path: __dirname + "/meta.png",
+              cid: "meta@",
+            },
+          ],
           html: `
-          <div>
-          <p><span style="font-weight:bold"> ${req.body.name}</span>,we welcome to our platform.</p>
-          <a style="background-color:yellow"color:white" href="https://logn-syst.herokuapp.com/user/verify/${token}">Verify Email</a>
-          <div style="backround-color:yellow;">
-          <p>Thanks and Regards</p>
-          <p>from google :)</p>
-    
-          </div>
-          </div>
+          <div style=" text-align: center;" >
+              <img src="cid:meta@" alt="meta"  
+              style=" width: 100%; height: 100px; margin-top: 10px;" />
+              <br />
+              <hr />
+              <br />
+              <p>
+              <span style=" color: rgb(99, 99, 99); font-weight: bold;">
+              ${req.body.name}</span>,we welcome to our platform :)
+               </p>
+               
+              <br />    
+              <a
+                style=" color:white !important;
+                padding:15px;
+                font-weight: 500;
+                background: rgb(0, 102, 255);
+                text-decoration: none;
+                border-radius: 10px;
+                box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;"
+                
+                href="https://logn-syst.herokuapp.com/user/verify/${token}"
+                >Verify Email</a>
+        
+              <p style="margin-top: 20px; color: gray;">
+              This link expire in 10 minutes</p>
+        
+              <br/>
+              <br/>
+              <p>Thanks and Regards</p>
+
+              <div style="margin-top: 50px;">&copy; 2021 Meta</div>
+            </div>
           `,
         });
         if (info) {
